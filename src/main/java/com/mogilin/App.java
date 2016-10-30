48ff951fb44ba97680942d0db8d092ecf0672b12
Ilya Mogilin <ilya.mogilin@phystech.edu>
2016-10-30 15:49:08 +0300

    First commit

---
+++ src/main/java/com/mogilin/App.java
@@ -0,0 +1,13 @@
+package com.mogilin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
