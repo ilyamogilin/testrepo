40130865345ef654cc8af176558cfdcd01b996c8
Ilya Mogilin <ilya.mogilin@phystech.edu>
2016-10-30 16:07:26 +0300

    Added simple JSoup code with sample HTML-file

--- src/main/java/com/mogilin/App.java
+++ src/main/java/com/mogilin/App.java
@@ -1,4 +1,10 @@
 package com.mogilin;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
 
 /**
  * Hello world!
@@ -6,8 +12,20 @@ package com.mogilin;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         System.out.println( "Hello World!" );
+        File input = new File("input.html");
+        Document doc = Jsoup.parse(input, "UTF-8", "http://google.com/");
+
+        Element content = doc.getElementById("content");
+        Elements links = content.getElementsByTag("a");
+        
+        for (Element link : links) {
+            String linkHref = link.attr("href");
+            String linkText = link.text();
+            System.out.println(linkHref + " " + linkText);
+        }
+
     }
 }
